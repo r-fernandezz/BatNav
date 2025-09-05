54d49e0fffc695ba9db18a1ff8c431015dd72672
Romain Fernandez <romain.fernandez@lougeneste.com>
2025-09-05 13:12:54 +0200

    ✏️ TYPO: change ggplot legend labels

--- R/get_map_PNR.r
+++ R/get_map_PNR.r
@@ -13,14 +13,31 @@
 get_map_PNR <- function(df_gpsRCT, PNR_shp) {
 
     gps_sf <- st_as_sf(df_gpsRCT, coords = c("Longitudedecimal", "Latitudedecimal"), crs = st_crs("EPSG:4326"))
+    
+    PNR_shp$Type <- factor(
+        PNR_shp$Type,
+        levels = c(
+            "Coeur du Parc national",
+            "Aire d'Adhésion",
+            "Aire ouverte à l'Adhésion"
+        )
+    )
+    
     ggplot() +
     geom_sf(data = PNR_shp, aes(fill = Type), alpha = 0.5) +
     geom_sf(data = gps_sf, aes(color = "Localisations GPS"), size = 1) +
-    scale_fill_manual(values = c(
-                        "Coeur du Parc national" = "#32913a",
-                        "Aire d'Adhésion" = "#15ff00",
-                        "Aire ouverte à l'Adhésion" = "#ffe600"
-                    )) +
+    scale_fill_manual(
+        values = c(
+            "Coeur du Parc national" = "#32913a",
+            "Aire d'Adhésion" = "#15ff00",
+            "Aire ouverte à l'Adhésion" = "#ffe600"
+        ),
+        labels = c(
+            "Coeur du Parc national" = "Cœur du parc",
+            "Aire d'Adhésion" = "Aire d'adhésion",
+            "Aire ouverte à l'Adhésion" = "Aire ouverte à l'adhésion"
+        ),
+    ) +
     scale_color_manual(values = c("Localisations GPS" =  "#ff0000")) +
     labs(fill = NULL, color = NULL) +
     theme_minimal() +
